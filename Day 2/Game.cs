8f6c518f00e1151936360c3e7589d54f86bf06d7
jostComp <jost@computas.com>
2023-12-02 20:48:23 +0100

    Day 2, pretty free

---
+++ Day 2/Game.cs
@@ -0,0 +1,19 @@
+﻿namespace Day_2;
+
+public class Game
+{
+    public int Id { get; }
+    public Round[] rounds { get; }
+
+    public Game(string gameInfo)
+    {
+        var split = gameInfo.Split(':');
+        Id = Int32.Parse(split[0].Split(' ')[1]);
+        var roundStrings = split[1].Split(';');
+        rounds = new Round[roundStrings.Length];
+        for (int i = 0; i < rounds.Length; i++)
+        {
+            rounds[i] = new Round(roundStrings[i]);
+        }
+    }
+}
---
+++ Day 2/Round.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_2;
+
+
+public class Round
+{
+    public int NumGreen { get; set; }
+    public int NumBlue { get; set; }
+    public int NumRed { get; set; }
+
+    public Round(string roundString)
+    {
+        string[] colorsInfo = roundString.Split(',');
+        foreach (var colorStr in colorsInfo)
+        {
+            var greenIndex = colorStr.IndexOf("green");
+            var blueIndex = colorStr.IndexOf("blue");
+            var redIndex = colorStr.IndexOf("red");
+            if (greenIndex != -1){
+                NumGreen = Int32.Parse(colorStr.Trim().Split(' ')[0]);
+            }
+            else if (blueIndex != -1){
+                NumBlue = Int32.Parse(colorStr.Trim().Split(' ')[0]);
+            }
+            else if (redIndex != -1){
+                NumRed = Int32.Parse(colorStr.Trim().Split(' ')[0]);
+            }
+        }
+    }
+}
